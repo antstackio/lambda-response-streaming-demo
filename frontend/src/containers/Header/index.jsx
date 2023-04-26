144f724d28b7a799ea0bf72fe281879770431e23
Sandy <sandeep@antstack.io>
2023-04-26 14:15:41 +0530

    feat: add header

---
+++ frontend/src/containers/Header/index.jsx
@@ -0,0 +1,29 @@
+import { Typography, Layout, Image, Divider } from "antd";
+import antstackLogo from "../../assets/antstack_type_logo.svg";
+const { Title } = Typography;
+import "./Header.css";
+const { Header } = Layout;
+
+const GenericHeader = () => (
+  <>
+    <Header className="header">
+      <a
+        href="https://www.antstack.com/"
+        target="_blank"
+        style={{ marginRight: "16px" }}
+      >
+        <Image src={antstackLogo} alt="antstack" width={256} preview={false} />
+      </a>
+      <div>
+        <Title style={{ margin: "auto" }} level={3}>
+          Revolutionizing Data Retrieval
+        </Title>
+        <Title style={{ margin: "auto" }} level={5}>
+          How Lambda Response Streaming APIs Beat Regular APIs
+        </Title>
+      </div>
+    </Header>
+    <Divider style={{ margin: 0 }} />
+  </>
+);
+export default GenericHeader;
