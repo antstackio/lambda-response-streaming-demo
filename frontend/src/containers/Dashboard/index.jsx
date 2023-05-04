d4769f0133c8e0268f9108da792e7f5f6a859505
Vishwasa Navada K <vishwasa.navada@sagerx.com>
2023-05-04 19:23:24 +0530

    feat: add two more sections for query table api calls

--- frontend/src/containers/Dashboard/index.jsx
+++ frontend/src/containers/Dashboard/index.jsx
@@ -1,18 +1,24 @@
-import { Col, Row } from "antd";
-import ResponseStreamTable from "../../features/ResponseStreamAPITable";
-import RegularTable from "../../features/RegularAPITable";
-import GenericHeader from "../Header";
-import "./Dashboard.css";
+import { Col, Row } from 'antd';
+import ResponseStreamTableScan from '../../features/ResponseStreamAPITableScan';
+import ResponseStreamTableQuery from '../../features/ResponseStreamAPITableQuery';
+import RegularTableScan from '../../features/RegularAPITableScan';
+import RegularTableQuery from '../../features/RegularAPITableQuery';
+import GenericHeader from '../Header';
+import './Dashboard.css';
 
 const Dashboard = () => (
   <>
     <GenericHeader />
     <Row>
-      <Col span={12} className="columnStyle columnLeft">
-        <ResponseStreamTable />
+      <Col span={12} className='columnStyle columnLeft'>
+        <ResponseStreamTableScan />
+        <br />
+        <ResponseStreamTableQuery />
       </Col>
-      <Col span={12} className="columnStyle columnRight">
-        <RegularTable />
+      <Col span={12} className='columnStyle columnRight'>
+        <RegularTableScan />
+        <br />
+        <RegularTableQuery />
       </Col>
     </Row>
   </>
