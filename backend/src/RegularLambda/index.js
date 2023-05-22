7f2fd53af0466275f9a08e0640c5e1bd4d190068
Sandy <sandeep@antstack.io>
2023-05-22 11:06:30 +0530

    Revert "WIP: Adds Query Table Sections and Related Backend Changes (#2)"
    This reverts commit cd9b00ed2881bc73b4d09c1607305fde8d1df9b7.

--- backend/src/RegularLambda/index.js
+++ backend/src/RegularLambda/index.js
@@ -1,37 +1,17 @@
-var coldStart = true;
-console.log('Cold Start');
-
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB();
-const scanTableName = 'sandy-scan-table2';
-const queryTableName = 'sandy-query-table2';
+const tableName = "sandy-scan-table2";
 
 exports.handler = async (event) => {
-  // NOTE: This is a make shift arrangement, might not be a reliable metric
-  let coldStartResponse;
-  if (coldStart) {
-    coldStartResponse = coldStart;
-  }
-  coldStart = false;
-
-  const queryParam = event.queryStringParameters;
-  console.log('🚀 ~ file: index.js:8 ~ exports.handler= ~ queryParam:', queryParam);
-
-  let counter = 0,
-    items;
-
-  if (queryParam?.query) {
-    items = await queryDynamoDBTable(queryTableName, 'bikes');
-  } else {
-    items = await scanDynamoDBTable(scanTableName);
-  }
+  let counter = 0;
+  const items = await scanDynamoDBTable(tableName);
 
   async function scanDynamoDBTable(tableName, startKey = null, items = []) {
     // Create a new Scan request with the table name and start key
     const params = {
       TableName: tableName,
       ExclusiveStartKey: startKey,
-      Limit: 300,
+      Limit: 250,
     };
 
     // Use the DynamoDB object to scan the table with the specified parameters
@@ -52,41 +32,13 @@ exports.handler = async (event) => {
 
     return items;
   }
-
-  async function queryDynamoDBTable(tableName, category, startKey = null, items = []) {
-    const params = {
-      TableName: tableName,
-      KeyConditionExpression: 'category = :category',
-      ExpressionAttributeValues: {
-        ':category': { S: category },
-      },
-      ExclusiveStartKey: startKey,
-      Limit: 300,
-    };
-
-    const data = await dynamodb.query(params).promise();
-
-    data.Items = data.Items.map((item) => {
-      return AWS.DynamoDB.Converter.unmarshall(item);
-    });
-
-    items = items.concat(data.Items);
-    counter += 1;
-
-    if (data.LastEvaluatedKey && counter < 10) {
-      return queryDynamoDBTable(tableName, category, data.LastEvaluatedKey, items);
-    }
-
-    return items;
-  }
-
   // End execution
   const response = {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Origin": "*",
     },
-    body: JSON.stringify({ items, coldStart: coldStartResponse }),
+    body: JSON.stringify(items),
   };
   return response;
 };
