3cf2661f5655a853195d51bf73c956809e586888
Sandy <sandeep@antstack.io>
2023-08-30 12:20:30 +0530

    bug fixes - lambda permission, api timeout, ddb loader (#11)

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -30,64 +30,26 @@ const apiCallStreamingResponse = (
               .then(({ done, value }) => {
                 // decode stream value
                 let chunk = decoder.decode(new Uint8Array(value));
+                tempChunk += chunk;
 
-                // helper function to set the data
-                const setTableData = () => {
-                  if (output.length === 0) {
-                    setTtfb((Date.now() - apiStartTime).toString() + " ms");
-                  } //statistics
+                if (output.length === 0) {
+                  setTtfb((Date.now() - apiStartTime).toString() + " ms");
+                }
 
-                  let result = JSON.parse(tempChunk); // assign
-                  result.map((e) => (e.key = e.id)); // add key to avoid ant design's unique key error
-                  output = output.concat(result); // add to previously loaded dataset
-                  tempChunk = ""; // clear temp value to start next iteration
-                  setData(output); // update data to refresh table
-                  setDdbItems(output.length); //statistics
+                // separate chunks and load data
+                if (tempChunk.includes("]")) {
+                  let tempChunkSplit = tempChunk.split("]");
+                  tempChunk = tempChunkSplit[1];
+                  output = [...output, ...JSON.parse(tempChunkSplit[0] + "]")];
+                  setData(output);
+                  setDdbItems(output.length);
                   setDdbItemsSize(
                     Math.round(JSON.stringify(output).length / 1024)
-                  ); //statistics
-                };
-
-                // identify separator and take action accordingly
-                if (chunk.includes(separator)) {
-                  //dataset 1 - ${separator}<stringData>${separator}
-                  if (chunk.indexOf(separator) === 0) {
-                    setTableData();
-                    chunk = chunk.replace(separator, ""); // after this it becomes dataset 2
-                  }
-                  // dataset 2 - <stringData${separator}<stringData>${separator}
-                  if (
-                    chunk.endsWith(separator) &&
-                    chunk.split(separator).length === 3
-                  ) {
-                    tempChunk += chunk.substring(0, chunk.indexOf(separator));
-                    setTableData();
-                  }
-                  // dataset 3 - <stringData>${separator}
-                  if (
-                    chunk.endsWith(separator) &&
-                    chunk.split(separator).length === 2
-                  ) {
-                    chunk = chunk.split(separator).filter((e) => e !== "");
-                    tempChunk += chunk[0];
-                    setTableData();
-                    chunk = ""; // to avoid execution for dataset 4
-                  }
-                  // dataset 4 - <stringData${separator}<stringData>
-                  if (chunk !== "") {
-                    chunk = chunk.split(separator).filter((e) => e !== "");
-                    tempChunk += chunk[0];
-                    setTableData();
-                    if (chunk.length > 1) tempChunk = chunk[1];
-                  }
+                  );
                 }
-                // if the separator does not exist, add it to temp chunk and continue the loop
-                else tempChunk += chunk;
 
-                // ending the iterative execution
                 if (done) {
                   controller.close();
-                  //statistics
                   setApiTime((Date.now() - apiStartTime).toString() + " ms");
                   return;
                 }
@@ -158,6 +120,11 @@ const chatGPTapiCallRegularResponse = (setData, value, setDataLoader) => {
     body: value,
   })
     .then((response) => {
+      if (response.status === 504) {
+        setData("## Request Timed Out");
+        setDataLoader(false);
+        throw "Timeout";
+      }
       return response.json();
     })
     .then((data) => {
